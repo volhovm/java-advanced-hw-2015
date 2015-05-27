45e2f4eb2b98024f110ae12fe3aa2683bcce2853
volhovM <volhovm.cs@gmail.com>
2015-05-27 21:26:41 +0300

    progress bar sketch added

--- src/ru/ifmo/ctddev/volhov/progressbar/UIFileCopy.java
+++ src/ru/ifmo/ctddev/volhov/progressbar/UIFileCopy.java
@@ -7,70 +7,98 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.*;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
 
 /**
  * @author volhovm
  *         Created on 5/21/15
  */
-public class UIFileCopy implements ActionListener {
-    private Path from, to;
+public class UIFileCopy extends JPanel implements ActionListener, PropertyChangeListener {
+    private CopyTask task;
     private JFrame frame;
     private JPanel panel;
-    private JLabel title;
+    private JLabel title, uptime, estimated, average, current;
     private JProgressBar progressBar;
     private JPanel statPanel;
     private JButton cancelButton;
 
     public UIFileCopy(String from, String to) {
-        this.from = Paths.get(from);
-        this.to = Paths.get(to);
+        Path fromP, toP;
+        fromP = Paths.get(from);
+        toP = Paths.get(to);
+        task = new CopyTask(fromP, toP);
         frame = new JFrame("UIFileCopy");
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 //        System.setProperty("awt.useSystemAAFontSettings","on");
 //        System.setProperty("swing.aatext", "true");
-        try {
-            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-        } catch (ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+//        } catch (ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException e) {
+//            e.printStackTrace();
+//        }
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+        System.setProperty("swing.aatext", "true");
         panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        panel.setLayout(new BorderLayout());
+
         title = new JLabel("Copying from in to out");
-        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(title, BorderLayout.PAGE_START);
+        title.setHorizontalAlignment(SwingConstants.CENTER);
         title.setBorder(new EmptyBorder(5, 5, 5, 5));
-        progressBar = new JProgressBar(0, 100);
-        progressBar.setValue(34);
-//        progressBar.setBorder(new EmptyBorder(5, 5, 5, 5));
+        title.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+//        progressBar.setAlignmentX(Component.CENTER_ALIGNMENT);
+
         statPanel = new JPanel();
-        statPanel.setLayout(new GridLayout(2, 2, 5, 5));
-        statPanel.add(new JLabel("Lol"));
-        statPanel.add(new JLabel("Lol1"));
-        statPanel.add(new JLabel("Lol2"));
-        statPanel.add(new JLabel("Lol3"));
+        statPanel.setLayout(new BoxLayout(statPanel, BoxLayout.PAGE_AXIS));
+        statPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        panel.add(statPanel);
+        panel.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+        statPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        progressBar = new JProgressBar(0, 100);
+        statPanel.add(progressBar);
+        progressBar.setValue(34);
+        progressBar.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        uptime = new JLabel("Uptime: aoeuaoeu");
+        statPanel.add(uptime);
+        uptime.setAlignmentX(Component.LEFT_ALIGNMENT);
+        uptime.setHorizontalAlignment(SwingConstants.LEFT);
+
+        estimated = new JLabel("Estimated: aoeu");
+        statPanel.add(estimated);
+        estimated.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        average = new JLabel("Average: aoeu");
+        statPanel.add(average);
+
+        current = new JLabel("Current: aoeu");
+        statPanel.add(current);
+
         cancelButton = new JButton("Cancel");
         cancelButton.addActionListener(this);
         cancelButton.setActionCommand("cancel");
-        cancelButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        cancelButton.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        panel.add(cancelButton, BorderLayout.PAGE_END);
 
         panel.setBorder(new EmptyBorder(10, 10, 10, 10));
-        panel.add(title);
-        panel.add(progressBar);
-        panel.add(statPanel);
-        panel.add(cancelButton);
-//        panel.setAlignmentX(Component.CENTER_ALIGNMENT);
-//        panel.setAlignmentY(Component.CENTER_ALIGNMENT);
         frame.setContentPane(panel);
     }
 
     void run() {
         frame.pack();
         frame.setVisible(true);
+        setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+        task.addPropertyChangeListener(this);
+        task.execute();
     }
 
     public static void main(String[] args) {
@@ -82,30 +110,71 @@ public class UIFileCopy implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         if (actionEvent.getActionCommand().equals("cancel")) {
-            JOptionPane.showMessageDialog(frame, null);
-            // cancel
+            JOptionPane.showMessageDialog(frame, "Closing application.");
+            task.cancel(true);
+            System.exit(0);
         }
     }
 
-    static class UIFileVisitor implements FileVisitor {
-        @Override
-        public FileVisitResult preVisitDirectory(Object o, BasicFileAttributes basicFileAttributes) throws IOException {
-            return null;
+    @Override
+    public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
+        if ("progress".equals(propertyChangeEvent.getPropertyName())) {
+            int progress = (Integer) propertyChangeEvent.getNewValue();
+            progressBar.setValue(progress);
+        }
+    }
+
+    private static class CopyTask extends SwingWorker<Void, Void> {
+        private Path from, to;
+
+        public CopyTask(Path from, Path to) {
+            this.from = from;
+            this.to = to;
         }
 
         @Override
-        public FileVisitResult visitFile(Object o, BasicFileAttributes basicFileAttributes) throws IOException {
+        protected Void doInBackground() throws Exception {
+            try (BufferedInputStream sin = new BufferedInputStream(new FileInputStream(from.toFile()));
+                 BufferedOutputStream sout = new BufferedOutputStream(new FileOutputStream(to.toFile()));
+            ) {
+                byte[] buf = new byte[4096];
+                int got = 0;
+                while (got != -1) {
+                    got = sin.read(buf);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+            setProgress(10);
+            while (getProgress() < 100) {
+                Thread.sleep(300);
+                setProgress(Math.min(100, getProgress() + 5));
+            }
             return null;
         }
 
-        @Override
-        public FileVisitResult visitFileFailed(Object o, IOException e) throws IOException {
-            return null;
-        }
+        private class UIFileVisitor implements FileVisitor {
+            @Override
+            public FileVisitResult preVisitDirectory(Object o, BasicFileAttributes basicFileAttributes)
+                    throws IOException {
+                return null;
+            }
 
-        @Override
-        public FileVisitResult postVisitDirectory(Object o, IOException e) throws IOException {
-            return null;
+            @Override
+            public FileVisitResult visitFile(Object o, BasicFileAttributes basicFileAttributes) throws IOException {
+                return null;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Object o, IOException e) throws IOException {
+                return null;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Object o, IOException e) throws IOException {
+                return null;
+            }
         }
     }
 }
